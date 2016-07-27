afa1f0860347ac0b5e909894f4470e8bb9b18509
dmspires <dmspires@us.ibm.com>
2016-07-27 17:00:47 -0500

    producution build

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     preLoaders: [
       {
         test: /\.js$/,
-        exclude: 'node_modules',
+        exclude: /node_modules/,
         loader: 'jshint-loader'
       }
     ],
